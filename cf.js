d3846db69923c4bf78d7e2f9c52df4b234189f41
karenmovsisyan <m.o.v.s@list.ru>
2017-07-06 15:17:07 +0400

    Update cf.js

--- cf.js
+++ cf.js
@@ -2,7 +2,13 @@ function cToF(c) {
   f = c * 9/5 + 32 
   return f
 }
+
 function fToC(f) {
   c = (f - 32)*5/9 
   return c
+}
+
+function speak(text) {
+  var msg = new SpeechSynthesisUtterance('Hello World');
+  window.speechSynthesis.speak(msg);
 }
